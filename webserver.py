829b3d8c33a99cbb97859377cad3ae3d1780724a
BuildTools <unconfigured@null.spigotmc.org>
2018-06-25 22:08:11 +0200

    Updating webserver draft

--- webserver.py
+++ webserver.py
@@ -1,6 +1,9 @@
-from flask import Flask
-app = Flask("Discord Awesome Stats")
+from flask import Flask, render_template, url_for
 
 @app.route('/')
 def root():
-    return ("Welcome")
+    return render_template("index.html.j2", server_channel_dict={})
+
+if __name__ == '__main__':
+    app = Flask("Discord Awesome Stats")
+    app.run(debug=True)

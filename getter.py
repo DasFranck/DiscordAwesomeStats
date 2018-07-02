ff5356d2a431539a82d61338b816f93aa08c9ce1
BuildTools <unconfigured@null.spigotmc.org>
2018-07-03 00:54:13 +0200

    getter.py transition to peewee (first part)

--- getter.py
+++ getter.py
@@ -3,14 +3,16 @@
 
 from datetime import datetime
 import asyncio
+import argparse
 import os
-import sqlite3
 import sys
 import time
 
 import discord
 import pytz
+import yaml
 
+from database import (Server, Channel, Member, Nick, Message, Message_count, database)
 from classes import Logger
 
 
@@ -25,52 +27,65 @@ class LogGetter(discord.Client):
 
         if not os.path.exists("data"):
             os.makedirs("data")
-        self.database = sqlite3.connect('data/database.db')
+        self.database = database
 
     async def on_ready(self):
         """
         Launch the getter when the bot is ready
         """
+        print("Sucessfully connected as %s (%s)\n" % (self.user.name, self.user.id))
+        self.logger.logger.info("Sucessfully connected as %s (%s)" % (self.user.name, self.user.id))
+        self.logger.logger.info("------------")
+
         await self.get_server_messages()
         await self.logout()
 
-    async def get_members_from_server(self, server):
+    async def get_metadata_from_server(self, server, config_server):
         """
         Get every members from the server provided and write it to the database.
-        Will create the table 'members_[server.id]' if it's not existing.
         """
-        cursor = self.database.cursor()
-        cursor.execute(
-            """
-            CREATE TABLE IF NOT EXISTS 'members_%s'(
-                id INTEGER PRIMARY KEY ON CONFLICT REPLACE UNIQUE,
-                name TEXT,
-                nick TEXT,
-                discriminator TEXT
-            )
-            """ % str(server.id)
-        )
+        self.database.connect()
+        self.database.create_tables([Server, Channel, Member, Nick])
 
-        members = []
+        nick_data = []
+        members_data = []
         for member in server.members:
-            members.append((
+            members_data.append((
                 member.id,
                 member.name,
-                member.nick,
                 member.discriminator
             ))
+            if member.nick:
+                nick_data.append((
+                    member.id,
+                    server.id,
+                    member.nick
+                ))
 
-        cursor.executemany(
-            """
-            INSERT INTO 'members_%s'(
-                id, name, nick, discriminator
-            )
-            VALUES(?, ?, ?, ?)
-            """ % server.id,
-            members
-        )
+        channels_data = []
+        for channel in server.channels:
+            if (channel.type != discord.ChannelType.voice and
+                ("channels" not in config_server or
+                channel.id in [str(i["id"]) for i in config_server["channels"]])):
+                channels_data.append((
+                    channel.id,
+                    channel.name,
+                    channel.server.id
+                ))
 
-        self.database.commit()
+        channel_fields = [Channel.id, Channel.name, Channel.server]
+        members_fields = [Member.id, Member.name, Member.discriminator]
+        nick_fields = [Nick.member_id, Nick.server_id, Nick.nick]
+
+        (Server.insert(id=server.id, name=server.name)).on_conflict_replace().execute()
+        (Channel.insert_many(channels_data, fields=channel_fields)).on_conflict_replace().execute()
+        print(len(members_data))
+        for idx in range(0, len(members_data), 300):
+            (Member.insert_many(members_data[idx:idx+300], fields=members_fields)).on_conflict_replace().execute()
+        for idx in range(0, len(nick_data), 300):
+            (Nick.insert_many(nick_data[idx:idx+300], fields=nick_fields)).on_conflict_replace().execute()
+
+        self.database.close()
 
     async def get_logs_from_channel(self, channel, cfg, timezone='UTC'):
         """
@@ -125,66 +140,49 @@ class LogGetter(discord.Client):
         cursor.execute("select count(*) from 'log_%s-%s'" % (str(cfg["id"]), channel.id))
         msg_count = cursor.fetchone()[0]
 
-        header = "Server name: " + cfg["name"] + "\n"
-        header += "Server ID: " + str(cfg["id"]) + "\n"
-        header += "Channel name: " + channel.name + "\n"
-        header += "Channel ID: " + channel.id + "\n"
-        header += channel.created_at.strftime("Created at: %A %d %b %Y %H:%M:%S UTC\n")
-        header += "Length: %d messages\n\n" % msg_count
-
-        self.summary.append({
-            "Server name": cfg["name"],
-            "Server ID": str(cfg["id"]),
-            "Channel name": channel.name,
-            "Channel ID": str(channel.id),
-            "Length": msg_count
-        })
-
-        print("\t\t%d" % msg_count)
-
     async def get_server_messages(self):
         """
         Get server messages by calling self.get_logs_from_channel on every marked chanel.
         """
         self.summary = []
 
-        print("Sucessfully connected as %s (%s)\n" % (self.user.name, self.user.id))
-        self.logger.logger.info("Sucessfully connected as %s (%s)" % (self.user.name, self.user.id))
-        self.logger.logger.info("------------")
-
         await self.change_presence(game=discord.Game(name="Getting logs..."))
-        for cfg in self.config["servers"]:
-            for server in self.servers:
-                if server.id == str(cfg["id"]):
-                    print("{} ({})".format(server.name, server.id))
-                    await self.get_members_from_server(server)
-                    for channel in server.channels:
-                        if (channel.type != discord.ChannelType.voice and
-                            ("channels" not in cfg or
-                            channel.id in [str(i["id"]) for i in cfg["channels"]])):
-                            try:
-                                await self.get_logs_from_channel(channel, cfg, self.config['timezone'])
-                            except discord.errors.Forbidden:
-                                pass
-                    print()
+
+        for config_server in self.config["servers"]:
+            server = discord.utils.get(self.servers, id=str(config_server["id"]))
+            if server is None:
+                print("{} was not found.\n".format(config_server["id"]))
+                continue
+            print("{} ({})".format(server.name, server.id))
+            await self.get_metadata_from_server(server, config_server)
+
+            for channel in server.channels:
+                if (channel.type != discord.ChannelType.voice and
+                    ("channels" not in config_server or
+                    channel.id in [str(i["id"]) for i in config_server["channels"]])):
+                    # Catch error if the bot have access to this channel
+                    try:
+                        continue
+                        await self.get_logs_from_channel(channel, config_server, self.config['timezone'])
+                    except discord.errors.Forbidden:
+                        pass
+            print()
+        
         print("Done.")
         self.logger.logger.info("Done.")
         self.logger.logger.info("#--------------END--------------#")
         return self.summary
 
-    def run(self, *args, **kwargs):
-        """
-        Run the bot without having any asyncio problem nor timeout.
-        """
-        try:
-            self.loop.run_until_complete(self.start(*args, **kwargs))
-        except KeyboardInterrupt:
-            self.loop.run_until_complete(self.logout())
-            pending = asyncio.Task.all_tasks()
-            gathered = asyncio.gather(*pending)
-            try:
-                gathered.cancel()
-                self.loop.run_until_complete(gathered)
-                gathered.exception()
-            except:
-                pass
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config_file", "-c", default="./config.yaml", help="Path to the config file")
+    args = parser.parse_args()
+
+    with open(args.config_file, 'r') as file:
+        config = yaml.load(file)
+
+    lg = LogGetter(config)
+    lg.run(config["token"])
+
+if __name__ == '__main__':
+    main()

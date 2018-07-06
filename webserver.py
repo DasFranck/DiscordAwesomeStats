c21ac4e6cb31af3b2090322cd1f09a3b37071ba0
DasFranck Hochstaetter <dasfranck@gmx.com>
2018-07-06 17:38:13 +0200

    Fix app flask (webserver.py)

--- webserver.py
+++ webserver.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template, url_for
 
+app = Flask("Discord Awesome Stats")
+
 @app.route('/')
 def root():
     return render_template("index.html.j2", server_channel_dict={})
 
 if __name__ == '__main__':
-    app = Flask("Discord Awesome Stats")
     app.run(debug=True)

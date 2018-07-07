1ffa7f1066547f9eb2098297b5ef2c563516e873
DasFranck Hochstaetter <dasfranck@gmx.com>
2018-07-07 21:55:30 +0000

    Date range added to both generator functions (generate_message_count.py)

--- generate_message_count.py
+++ generate_message_count.py
@@ -8,9 +8,10 @@ import os
 import sys
 import time
 
+import arrow
 import discord
 from peewee import fn
-import pytz
+from pendulum import period
 import yaml
 
 from database import (Server, Channel, Member, Message,
@@ -25,14 +26,51 @@ class MessageCountGenerator():
         if 'timezone' not in self.config:
             self.config['timezone'] = 'UTC'
 
-    def generate_message_count_per_user_per_channel(self):
-        print("\t\tGenerating message count per user... ", end='')
+    def generate_message_count_per_user_per_channel(self, channel_id, reset=False):
+        """
+        Generate the message count per user for channel_id for each day,
+        Skip if no message exist for this channel
 
+        reset means to recalculate everything
+        """
+        print("\t\tGenerating message count per user... ", end='')
+        self.database.create_tables([MessageCountUser])
+
+        today = arrow.utcnow().to(self.config['timezone']).date()
+        last_count = MessageCountUser.select(fn.MAX(MessageCountUser.date)).where(MessageCountUser.channel_id == channel_id).scalar()
+        if reset or not last_count:
+            first_message_timestamp = Message.select(fn.MIN(Message.timestamp)).where(Message.channel_id == channel_id).scalar()
+            if not first_message_timestamp:
+                print("skipped.")
+                return
+            first_message_date = arrow.get(first_message_timestamp).to(self.config['timezone']).date()
+            date_range = period(first_message_date, today).range("days")
+        else:
+            date_range = period(last_count, today).range("days")
         print("done.")
         pass
 
-    def generate_message_count_per_channel(self):
+    def generate_message_count_per_channel(self, channel_id, reset=False):
+        """
+        Generate the message count per user for each day,
+        Skip if no message exist for this channel
+
+        reset means to recalculate everything
+        """
         print("\t\tGenerating message count of this channel... ", end='')
+        self.database.create_tables([MessageCountChannel])
+
+        today = arrow.utcnow().to(self.config['timezone']).date()
+        last_count = MessageCountChannel.select(fn.MAX(MessageCountChannel.date)).where(MessageCountChannel.channel_id == channel_id).scalar()
+        if reset or not last_count:
+            first_message_timestamp = Message.select(fn.MIN(Message.timestamp)).where(Message.channel_id == channel_id).scalar()
+            if not first_message_timestamp:
+                print("skipped.")
+                return
+            first_message_date = arrow.get(first_message_timestamp).to(self.config['timezone']).date()
+            date_range = period(first_message_date, today).range("days")
+        else:
+            date_range = period(last_count, today).range("days")
         
         print("done.")
         pass
@@ -48,8 +86,8 @@ class MessageCountGenerator():
                 if ("channels" not in config_server or
                     channel.id in [i["id"] for i in config_server["channels"]]):
                         print("\t{} ({})".format(channel.name, channel.id))
-                        self.generate_message_count_per_channel()
-                        self.generate_message_count_per_user_per_channel()
+                        self.generate_message_count_per_user_per_channel(channel.id)
+                        self.generate_message_count_per_channel(channel.id)
             print()
 
         self.database.close()

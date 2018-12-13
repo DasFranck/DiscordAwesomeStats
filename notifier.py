514e27682fa3f4ec3d5b8fabd578133dd7778d3a
DasFranck Hochstaetter <dasfranck@gmx.com>
2018-12-13 00:02:01 +0000

    Prettify messqge sent by notifier.py

--- notifier.py
+++ notifier.py
@@ -19,17 +19,21 @@ class Notifier(discord.Client):
         self.database = database
 
     async def notify(self):
+        """
+        """
         await self.change_presence(activity=discord.Game(name="Notifying peoples..."))
-
         self.database.connect()
+
         for config_guild in self.config["guilds"]:
             print(f"Generating leaderboards counts for {config_guild['id']}... ", end='', flush=True)
+            # Skip if the config specify to not notify this guild
             if "silent" in config_guild and config_guild["silent"]:
                 print("skipped")
                 continue
 
             print()
 
+            # Get channel list to notify for this guild
             if "report_all" in config_guild and config_guild["report_all"]:
                 channels_id = [channel.id for channel in Channel.select().where(Channel.guild_id == config_guild["id"])]
             else:
@@ -39,12 +43,21 @@ class Notifier(discord.Client):
                 print(f"\t{channel_id}")
                 channel = self.get_channel(channel_id)
                 leaderboard = []
+                # Get top 10 user
                 for i in MessageCountUserChannel.select().where((MessageCountUserChannel.channel_id == channel_id) & (MessageCountUserChannel.date == str(datetime.datetime.now().date() - datetime.timedelta(days=1)))).order_by(MessageCountUserChannel.count.desc()):
+                    if i.count == 0:
+                        break
                     leaderboard.append((i.author_id, i.count))
 
-                message = f"Leaderboards for #{channel_id} ({config_guild['id']})\n"
-                for (i, (user_id, count)) in enumerate(leaderboard[:10]):
-                    message += f"{user_id}: {count}\n"
+                message = f"***Leaderboards for #{str(channel)} ({str(channel.guild)}):***\n\n"
+                if len(leaderboard):
+                    for (i, (user_id, count)) in enumerate(leaderboard[:10]):
+                        user = Member.get(Member.id == user_id)
+                        message += f"**{i + 1}**. with {count} messages: **{user.name}**#{user.discriminator}\n"
+
+                else:
+                    message += "Nothing has been posted here since last time, that's pretty sad... :/"
+
                 try:
                     await channel.send(message)
                 except:

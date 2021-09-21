e56b3e524db7c4488a4809fbc94a699f281dbec7
DasFranck <dasfranck@gmx.com>
2021-09-22 01:10:23 +0200

    Fix webserver.frontend.guild_id fetching

--- webserver/frontend.py
+++ webserver/frontend.py
@@ -1,4 +1,5 @@
 from contextlib import closing
+from typing import OrderedDict
 
 from flask import Blueprint, render_template, flash, redirect, url_for
 from markupsafe import escape
@@ -33,13 +34,22 @@ def guild():
 
 @frontend.route("/guild/<int:guild_id>")
 def guild_id(guild_id: int):
-    with closing(get_db().cursor()) as cursor:
-        for channel_id in cursor.execute("SELECT channel_id FROM channel WHERE guild_id = ?", (guild_id,)).fetchall()[0]:
-            message_count_per_date = cursor.execute("SELECT date, SUM(count), member_id FROM daily_message_count WHERE channel_id = ? GROUP BY date;", (channel_id,)).fetchall()
+    message_count_per_date = OrderedDict()
 
-    date_axis = [str(message_count[0]) for message_count in message_count_per_date]
-    count_axis = [message_count[1] for message_count in message_count_per_date]
-    return render_template('guild_id.html.j2', target_guild=get_guild(guild_id), date_axis=date_axis, count_axis=count_axis)
+    with closing(get_db().cursor()) as cursor:
+        for channel_id in cursor.execute("SELECT channel_id FROM channel WHERE guild_id = ?", (guild_id,)).fetchall():
+            for count in cursor.execute("SELECT date, SUM(count), member_id FROM daily_message_count WHERE channel_id = ? GROUP BY date;", (channel_id[0],)).fetchall():
+                date = str(count[0])
+                if date in message_count_per_date:
+                    message_count_per_date[date] += count[1]
+                else:
+                    message_count_per_date[date] = count[1]
+
+    return render_template(
+        'guild_id.html.j2', 
+        target_guild=get_guild(guild_id), 
+        message_count_per_date=message_count_per_date, 
+    )
 
 @frontend.route("/user/")
 def user():

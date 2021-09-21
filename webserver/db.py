7b81fc0ce33cae76dff480364271448a19aedc91
DasFranck <dasfranck@gmx.com>
2021-09-21 18:46:22 +0200

    Update db path in webserver.db

--- webserver/db.py
+++ webserver/db.py
@@ -6,7 +6,7 @@ from flask import g
 def get_db():
     if 'db' not in g:
         g.db = sqlite3.connect(
-            "../das.db",
+            "das.db",
             detect_types=sqlite3.PARSE_DECLTYPES
         )
         g.db.row_factory = sqlite3.Row
